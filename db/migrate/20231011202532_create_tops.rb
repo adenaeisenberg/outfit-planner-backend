af6de219fa14742cbe81122690ac9c46d62040dd
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-11 16:25:48 -0400

    added top model

---
+++ db/migrate/20231011202532_create_tops.rb
@@ -0,0 +1,10 @@
+class CreateTops < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tops do |t|
+      t.string :name
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
