131f70807341a588f1a0dc29b445bdf78d0eee25
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-12 15:02:22 -0400

    created outfit model

---
+++ db/migrate/20231012190108_create_outfits.rb
@@ -0,0 +1,11 @@
+class CreateOutfits < ActiveRecord::Migration[7.0]
+  def change
+    create_table :outfits do |t|
+      t.string :day
+      t.integer :top_id
+      t.integer :bottom_id
+
+      t.timestamps
+    end
+  end
+end
