3137e910ac8a038f3741f9145d57a8002dfdaca8
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-23 16:21:30 -0400

    added a validation that an outfit won't save if there isn't a day, top, or bottom

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,4 +1,8 @@
 class Outfit < ApplicationRecord
+  validates :day, presence: true
+  validates :top, presence: true
+  validates :bottom, presence: true
+
   belongs_to :user
   belongs_to :bottom
   belongs_to :top
