da63fdf3cd51c1f4f19d1f90db2c91aa29ecb205
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-25 16:40:05 -0400

    updated the associations- bottoms now belong to user

--- app/models/bottom.rb
+++ app/models/bottom.rb
@@ -1,3 +1,4 @@
 class Bottom < ApplicationRecord
   has_many :tops, :through => :outfits
+  belongs_to :user 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
 
   has_many :outfits 
   has_many :tops
+  has_many :bottoms
 end
