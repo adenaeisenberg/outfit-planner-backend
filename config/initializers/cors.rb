ef37ef3d1989aedde22c512b43f8e7320b35ef08
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-11-07 16:50:35 -0500

    adds google fonts to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "example.com", "localhost:5173"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
+
+  allow do
+    origins "fonts.googleapis.com"
+    resource "*", headers: :any, methods: [:get]
+  end
 end
